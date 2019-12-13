92400e6b9e9993d682cbab20616f42fb09f56107
hoa.phan <hoa.phan@foxsports.com.au>
2019-12-14 10:56:36 +1100

    sav

--- es6-persistence/src/main/java/com/netflix/conductor/elasticsearch/EmbeddedElasticSearchProvider.java
+++
@@ -1,7 +0,0 @@
-package com.netflix.conductor.elasticsearch;
-
-import javax.inject.Provider;
-import java.util.Optional;
-
-public interface EmbeddedElasticSearchProvider extends Provider<Optional<EmbeddedElasticSearch>> {
-}
--- es6-persistence/src/main/java/com/netflix/conductor/elasticsearch/SystemPropertiesElasticSearchConfiguration.java
+++
@@ -1,7 +0,0 @@
-package com.netflix.conductor.elasticsearch;
-
-import com.netflix.conductor.core.config.SystemPropertiesConfiguration;
-
-public class SystemPropertiesElasticSearchConfiguration
-        extends SystemPropertiesConfiguration implements ElasticSearchConfiguration {
-}
