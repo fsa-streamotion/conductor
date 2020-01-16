6a00861c6d9ae251446695d637eb0153736eac0e
hoa.phan <hoa.phan@foxsports.com.au>
2020-01-16 14:00:27 +1100

    safely verify secret extraction on the side
    save

--- mysql-persistence/src/main/java/com/netflix/conductor/mysql/MySQLDataSourceProvider.java
+++ mysql-persistence/src/main/java/com/netflix/conductor/mysql/MySQLDataSourceProvider.java
@@ -1,5 +1,14 @@
 package com.netflix.conductor.mysql;
 
+import com.amazonaws.services.secretsmanager.AWSSecretsManager;
+import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder;
+import com.amazonaws.services.secretsmanager.model.DecryptionFailureException;
+import com.amazonaws.services.secretsmanager.model.GetSecretValueRequest;
+import com.amazonaws.services.secretsmanager.model.GetSecretValueResult;
+import com.amazonaws.services.secretsmanager.model.InternalServiceErrorException;
+import com.amazonaws.services.secretsmanager.model.InvalidParameterException;
+import com.amazonaws.services.secretsmanager.model.InvalidRequestException;
+import com.amazonaws.services.secretsmanager.model.ResourceNotFoundException;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
@@ -10,6 +19,7 @@ import org.slf4j.LoggerFactory;
 import javax.inject.Inject;
 import javax.inject.Provider;
 import javax.sql.DataSource;
+import java.util.Base64;
 import java.util.concurrent.ThreadFactory;
 
 public class MySQLDataSourceProvider implements Provider<DataSource> {
@@ -39,6 +49,8 @@ public class MySQLDataSourceProvider implements Provider<DataSource> {
     }
 
     private HikariConfig createConfiguration() {
+        logger.info("create HikariConfig...");
+        getSecret("/secret/conductor");
         HikariConfig cfg = new HikariConfig();
         cfg.setJdbcUrl(configuration.getJdbcUrl());
         cfg.setUsername(configuration.getJdbcUserName());
@@ -68,8 +80,6 @@ public class MySQLDataSourceProvider implements Provider<DataSource> {
             logger.debug("Flyway migrations are disabled");
             return;
         }
-
-
         Flyway flyway = new Flyway();
         configuration.getFlywayTable().ifPresent(tableName -> {
             logger.debug("Using Flyway migration table '{}'", tableName);
@@ -79,5 +89,34 @@ public class MySQLDataSourceProvider implements Provider<DataSource> {
         flyway.setDataSource(dataSource);
         flyway.setPlaceholderReplacement(false);
         flyway.migrate();
+    }
+
+
+    public static void getSecret(String secretName) {
+        logger.info("getSecret:[{}]", secretName);
+        String region = "ap-southeast-2";
+
+        AWSSecretsManager client  = AWSSecretsManagerClientBuilder.standard()
+                .withRegion(region)
+                .build();
+
+        String secret, decodedBinarySecret;
+        GetSecretValueRequest getSecretValueRequest = new GetSecretValueRequest().withSecretId(secretName);
+        GetSecretValueResult getSecretValueResult = null;
+        logger.info("getSecretValueRequest:[{}]", getSecretValueRequest);
+        try {
+            getSecretValueResult = client.getSecretValue(getSecretValueRequest);
+            logger.info("getSecretValueResult:[{}]", getSecretValueResult);
+            if (getSecretValueResult.getSecretString() != null) {
+                secret = getSecretValueResult.getSecretString();
+                logger.info("secret:[{}]", secret);
+            }
+            else {
+                decodedBinarySecret = new String(Base64.getDecoder().decode(getSecretValueResult.getSecretBinary()).array());
+                logger.info("decodedBinarySecret:[{}]", decodedBinarySecret);
+            }
+        } catch (DecryptionFailureException | InternalServiceErrorException | InvalidParameterException | InvalidRequestException | ResourceNotFoundException e) {
+            logger.error("error getting secret ", e);
+        }
     }
 }
